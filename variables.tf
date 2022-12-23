1cf9124c144ab0d53a39298f2244c444b7af02db
Sven Jansen <sven.jansen@devicetrust.com>
2022-12-23 15:11:25 +0100

    .

---
+++ variables.tf
@@ -0,0 +1,19 @@
+#####
+# Variables for administrative connection to the ADC
+#####
+
+variable adc-base{
+}
+
+#####
+# Variables for license file upload
+#####
+
+variable adc-lic-filename {
+}
+
+variable adc-lic-filelocation {
+}
+
+variable adc-lic-filecontent {
+}
