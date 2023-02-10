6148c571e749f8c97294243d1e9bb2185f85ce4d
Sven Jansen <sven.jansen@devicetrust.com>
2023-02-10 17:09:02 +0100

    .

--- main.tf
+++ main.tf
@@ -8,6 +8,18 @@ resource "citrixadc_systemfile" "license_upload" {
     filecontent  = file(var.adc-license.filecontent)
 }
 
+output "filename" {
+  value = var.adc-license.filename
+}
+
+output "filelocation" {
+  value = var.adc-license.filelocation
+}
+
+output "filecontent" {
+  value = var.adc-license.filecontent
+}
+
 #####
 # Save Configuration
 #####
